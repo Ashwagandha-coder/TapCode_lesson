40e870f6ed490974290737f86b7b7cbc49eef15d
Ashwagandha-coder <max.cacz@yandex.ru>
2022-08-08 21:10:48 +0500

    lesson_13.kt done

--- src/main/kotlin/lesson_13.kt
+++ src/main/kotlin/lesson_13.kt
@@ -1,2 +1,42 @@
-class lesson_13 {
+fun main() {
+
+    val dog = Dog()
+
+    dog.sound()
+}
+
+
+class Dog: Animal() {
+
+    override fun sound() {
+        println("Dog sound")
+    }
+
+}
+class Cat: Animal() {
+
+    override fun sound() {
+        println("Cat sound")
+    }
+
+
+}
+
+open class Animal {
+
+
+    open fun sound() {
+
+        println("Animal sound")
+
+    }
+
+
+    open fun eat() {
+
+        println("Animal eat")
+
+    }
+
+
 }
