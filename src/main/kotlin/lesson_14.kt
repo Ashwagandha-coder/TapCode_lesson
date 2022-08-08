1b8497e87e4062a59ed2d8887b80de68b161831b
Ashwagandha-coder <max.cacz@yandex.ru>
2022-08-08 22:22:13 +0500

    lesson_14.kt done

--- src/main/kotlin/lesson_14.kt
+++ src/main/kotlin/lesson_14.kt
@@ -1,2 +1,40 @@
-class lesson_14 {
+fun main() {
+
+    // public private protected
+
+    val laptop = Laptop("Mechanical")
+
+    laptop.yrt = 22
+
+    println(laptop.yrt)
 }
+
+fun otherPoroh() {}
+
+
+class Laptop (private val keyboard: String) {
+
+    private var some: String = ""
+        get() { return field }
+        //set(some: String) {this.some = some}
+
+    private val otherSome: String = ""
+        get() {return field}
+
+    var yrt: Int = 0
+        get() = field
+
+
+    fun doSome() {
+
+        yrt = 50
+        some = "dgfsd"
+    }
+
+}
+
+annotation class Urka {}
+
+open class Furniture {}
+class Chair: Furniture() {}
+class Table: Furniture() {}
