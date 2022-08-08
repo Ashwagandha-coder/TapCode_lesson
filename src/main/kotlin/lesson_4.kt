508fe6d49cba15d899f27f9eb563fbbabb0b88e6
Ashwagandha-coder <max.cacz@yandex.ru>
2022-08-08 14:41:05 +0500

    lesson_4.kt done

--- src/main/kotlin/lesson_4.kt
+++ src/main/kotlin/lesson_4.kt
@@ -1,2 +1,37 @@
-class lesson_4 {
+fun main() {
+
+    var flag = false
+
+    while(!flag) {
+
+        println("File loading")
+        flag = true
+
+    }
+
+    flag = false
+
+    do {
+
+        println("Do while")
+        flag = true
+
+    }
+        while (!flag)
+
+        for (i in 1..10 step 2) {
+
+            println(i)
+
+        }
+
+    println("---")
+
+    for (i in 100 downTo 1 step 2)
+        println("$i + People")
+
+    var param: Int
+
+
 }
+
