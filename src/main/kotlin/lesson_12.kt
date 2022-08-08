37bfd86238c390a9b5af920400857fc6cbe4df1e
Ashwagandha-coder <max.cacz@yandex.ru>
2022-08-08 20:57:39 +0500

    1\2

--- src/main/kotlin/lesson_12.kt
+++ src/main/kotlin/lesson_12.kt
@@ -1,2 +1,44 @@
-class lesson_12 {
+fun main() {
+
+    val variable = Car(speed = 56, name = "Buggati Veiron", windows = 4, floors = 2, engine = "Jz")
+
+    variable.go()
+
+    println(variable.power)
+
+
+
+
+
 }
+
+public class Car (
+    private val engine: String,
+    private val floors: Int,
+    private val windows: Int,
+    private val name: String,
+    private val speed: Int) {
+
+    var power = 98
+        get() {
+            return field
+        }
+        set(value) {
+            power = value
+        }
+
+
+    fun go() {
+
+        power = 22
+
+    }
+
+    fun stop() {}
+
+
+
+}
+
+// недоделал
+
