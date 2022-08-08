312dc116470aebdb5a2dcced058dedba8cfafd38
Ashwagandha-coder <max.cacz@yandex.ru>
2022-08-08 19:07:38 +0500

    lesson_9.kt and lesson_9_2.kt done

--- src/main/kotlin/lesson_9.kt
+++ src/main/kotlin/lesson_9.kt
@@ -1,2 +1,47 @@
-class lesson_9 {
+fun main() {
+
+    var param1: Double? = readln()?.toDoubleOrNull()
+    var param2: String? = readLine()
+    var param3: Double? = readln()?.toDoubleOrNull()
+
+
+    var result: Double? = 0.0
+
+    if (param1 == null || param2 == null || param3 == null) {
+
+        print("Invalid data")
+    }
+    else {
+
+        when(param2) {
+
+            "+" -> result = sumNumbers(param1,param3)
+            "-" -> result = difference(param1,param3)
+            "*" -> result = multyply(param1,param3)
+            "/" -> result =  delimetr(param1,param3)
+
+
+
+        }
+
+
+    }
+
+    println(result?.toInt())
+
+
+
+
+
 }
+
+fun sumNumbers(a: Double?, b: Double): Double = a?.plus(b)!!
+
+fun multyply(a: Double, b: Double): Double = a * b
+
+fun delimetr(a: Double, b: Double): Double = a / b
+
+fun difference(a: Double, b: Double): Double = a - b
+
+
+
