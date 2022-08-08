854192cc9df35a468147b713f9e1fe7e4d0c59af
Ashwagandha-coder <max.cacz@yandex.ru>
2022-08-08 13:26:36 +0500

    lesson_3.kt done

--- src/main/kotlin/lesson_3.kt
+++ src/main/kotlin/lesson_3.kt
@@ -1,2 +1,40 @@
-class lesson_3 {
+import java.io.PrintStream
+import java.io.UTFDataFormatException
+import java.nio.charset.StandardCharsets
+
+fun main() {
+
+    var age: Int
+
+    age = 20
+
+    // if else
+
+    if(age >= 18) {
+        println("Добро пожалолвать")
+    }
+    else {
+        println("В доступе отказано")
+    }
+
+    val dayOfWeek: String
+
+    dayOfWeek = "Monday"
+
+    // when
+
+
+    when(dayOfWeek) {
+
+        "Monday" -> println("Monday")
+        "Tuesday" -> println("Tuesday")
+        "Sunday" -> println("Sunday")
+        "Saturday" -> println("Saturday")
+    }
+
+    val russia: PrintStream = PrintStream(System.out,true,StandardCharsets.UTF_8)
+
+    russia.println("Россия")
+
+
 }
